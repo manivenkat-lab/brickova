f7f4acd4bc8b8362e66a422feae338750a005924
Pangoth Hemanth Nayak <hemanthnayakpangoth@gmail.com>
2026-06-13 17:05:12 +0530

    fix: fallback to actual firebase keys for vercel deployment

--- firebase.ts
+++ firebase.ts
@@ -4,13 +4,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "AIzaSyCTjtDpPGgSSDqNY3Awln7SwuqD6Xn7mJE",
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "m-homes-bad1d.firebaseapp.com",
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "m-homes-bad1d",
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "m-homes-bad1d.appspot.com",
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "504290879014",
+  appId: import.meta.env.VITE_FIREBASE_APP_ID || "1:504290879014:web:b433dfd7cb03ed2a4e28e7",
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID || ""
 };
 
 const app = initializeApp(firebaseConfig);
